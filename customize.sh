c165d80f4f7cd37fc3b36a782c193d433e29ddd1
Shahmi Saidi <shahmi@technosparks.net>
2022-01-24 02:18:00 +0800

    properly apply permissions

--- customize.sh
+++ customize.sh
@@ -14,7 +14,8 @@ unzip -o "$ZIPFILE" -x 'META-INF/*' -d $MODPATH >&2
 
 # Default permissions
 set_perm_recursive $MODPATH 0 0 0755 0644
-set_perm_recursive $MODPATH/system/bin 0 2000 0755 0755
+set_perm_recursive $MODPATH/arm 0 2000 0755 0755
+set_perm_recursive $MODPATH/arm64 0 2000 0755 0755
 #set_perm_recursive $MODPATH/system/xbin 0 2000 0755 0755
 #set_perm_recursive $MODPATH/system/system_ext/bin 0 2000 0755 0755
 #set_perm_recursive $MODPATH/system/vendor/bin 0 2000 0755 0755 u:object_r:vendor_file:s0
